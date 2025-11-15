2f33b79beb5fa7a80e51092b88366cf859a09cc7
cole-propach <colepropach@gmail.com>
2025-11-14 19:47:01 -0800

    decreased depth for client

--- connect4/connect4.js
+++ connect4/connect4.js
@@ -334,7 +334,7 @@ function simulate(){
         if(turn === 'gold' && !aiThinking){
             aiThinking = true;
             (async () => {
-                let result = await runExe(moves, 10);
+                let result = await runExe(moves, 8);
                 result = result.trim(); //remove newlines and spaces
                 playMove(result);
                 aiThinking = false;
