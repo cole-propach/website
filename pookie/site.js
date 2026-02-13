843ab6bfb1aa8973257d7277061ef142023b9e8b
cole-propach <colepropach@gmail.com>
2026-02-12 23:00:09 -0800

    p

---
+++ pookie/site.js
@@ -0,0 +1,34 @@
+import { messages } from "./messages.js";
+import { images } from "./images.js";
+
+let messageNumber = getRandomInt(0, messages.length-1);
+let imageNumber = getRandomInt(0, images.length-1);
+document.getElementById("message").textContent = messages[messageNumber];
+document.getElementById("display").src = "/images/"+images[imageNumber];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function newRandomMessage(){
+    let oldNumber = messageNumber;
+    while(messageNumber == oldNumber){
+        messageNumber = getRandomInt(0, messages.length-1);
+    }
+    document.getElementById("message").textContent = messages[messageNumber];
+}
+
+function newRandomImage(){
+  let oldNumber = imageNumber;
+    while(imageNumber == oldNumber){
+        imageNumber = getRandomInt(0, images.length-1);
+    }
+    document.getElementById("display").src = "/images/"+images[imageNumber];
+}
+
+const refreshButton = document.getElementById("refreshButton");
+
+refreshButton.addEventListener("click", () => {
+  newRandomMessage();
+  newRandomImage();
+});
